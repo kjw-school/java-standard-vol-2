8cb97e6089f53fd469297eb62502fde731369417
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2024-11-06 00:13:01 +0900

    Study Day 8

--- src/chapter13/Chapter13_3.java
+++ src/chapter13/Chapter13_3.java
@@ -29,4 +29,34 @@ public class Chapter13_3 {
      * 쓰레드는 '사용자 쓰레드(user thread)'와 '데몬 쓰레드(daemon thread)'두 종류가 있다.
      */
 
+    /**
+     * 새로 생성한 쓰레드에서 고의로 예외를 발생시키고 printStackTrace()를 이용해서 예외가 발생한 당시의 호출스택을 출력하는 예제이다.
+     * 호출스택의 첫 번째 메서드가 main메서드가 아니라 run메서드인 것을 확인하자.
+     * 한 쓰레드가 예외가 발생해서 종료되어도 다른 쓰레드의 실행에는 영향을 미치지 않는다.
+     * main쓰레드이 호출스택이 없는 이유는 main쓰레드가 이미 종료되었기 때문이다.
+     */
+    static class ThreadEx2 {
+
+        public static void main(String[] args) {
+            ThreadEx2_1 t1 = new ThreadEx2_1();
+            t1.start();
+        }
+
+    }
+
+    static class ThreadEx2_1 extends Thread {
+        public void run() {
+            throwException();
+        }
+
+        public void throwException() {
+
+            try {
+                throw new Exception();
+            } catch(Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
