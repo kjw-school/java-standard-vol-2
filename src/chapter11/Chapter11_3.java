898426f9451c17f4633b716dd51ae3f248e8ec24
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2024-11-19 01:13:17 +0900

    Study Day 9

--- src/chapter11/Chapter11_3.java
+++ src/chapter11/Chapter11_3.java
@@ -1,5 +1,7 @@
 package chapter11;
 
+import java.util.*;
+
 /**
  * 1.3 LinkedList
  */
@@ -35,6 +37,87 @@ public class Chapter11_3 {
      *     Node previous; // 이전 요소의 주소를 저장
      *     Object obj; // 데이터를 저장
      * }
+     * 더블 링크드 리스트의 접근성을 보다 향상시킨 것이 '더블 써큘러 링크드 리스트(이중 원형 연결리스트, doubly circular linked list)'
+     * 인데, 단순히 더블 링크드 리스트의 첫 번째 요소와 마지막 요소를 서로 연결시킨 것이다. 이렇게 하면, 마지막 요소의 다음요소가 첫 번째
+     * 요소가 되고, 첫 번째 요소의 이전 요소가 마지막 요소가 된다.
+     * 실제로 LinkedList클래스는 이름과 달리 '링크드 리스트'가 아닌 '더블 링크드 리스트'로 구현되어 있는데, 이는 링크드 리스트의 단점인
+     * 낮은 접근성(accessbility)을 높이기 위한 것이다.
+     * LinkedList 역시 List인터페이스를 구현했기 때문에 ArrayList와 내부구현방법만 다를뿐 제공하는 메서드의 종류와 기능은 거의 같기 때문
+     * 에 이에대한 설명은 따로 필요없을 것 같다.
+     */
+
+    static class ArrayListLinkedListTest {
+
+        public static void main(String[] args) {
+
+            //추가할 데이터의 개수를 고려하여 충분히 잡아야한다.
+            ArrayList al = new ArrayList(2000000);
+            LinkedList ll = new LinkedList();
+
+            System.out.println("= 순차적으로 추가하기 =");
+            System.out.println("ArrayList :"+add1(al));
+            System.out.println("LinkedList :"+add1(ll));
+            System.out.println();
+            System.out.println("= 중간에 추가하기 =");
+            System.out.println("ArrayList :"+add2(al));
+            System.out.println("LinkedList :"+add2(ll));
+            System.out.println();
+            System.out.println("= 중간에서 삭제하기 =");
+            System.out.println("ArrayList :"+remove2(al));
+            System.out.println("LinkedList :"+remove2(ll));
+            System.out.println();
+            System.out.println("= 순차적으로 삭제하기 =");
+            System.out.println("ArrayList :"+remove1(al));
+            System.out.println("LinkedList :"+remove1(ll));
+
+        }
+
+        public static long add1(List list) {
+
+            long start = System.currentTimeMillis();
+            for(int i =0; i < 1000000; i++) list.add(i+"");
+            long end = System.currentTimeMillis();
+            return end - start;
+
+        }
+
+        public static long add2(List list) {
+
+            long start = System.currentTimeMillis();
+            for(int i =0; i < 10000; i++) list.add(500, "X");
+            long end = System.currentTimeMillis();
+            return end - start;
+
+        }
+
+        public static long remove1(List list) {
+            long start = System.currentTimeMillis();
+            for(int i=list.size()-1; i >= 0; i--) list.remove(i);
+            long end = System.currentTimeMillis();
+            return end - start;
+        }
+
+        public static long remove2(List list) {
+            long start = System.currentTimeMillis();
+            for(int i= 0 ; i < 10000; i++) list.remove(i);
+            long end = System.currentTimeMillis();
+            return end - start;
+        }
+
+    }
+
+    /**
+     * 결론1. 순차적으로 추가/삭제하는 경우에는 ArrayList가 LinkedList보다 빠르다.
+     * 만일 ArrayList의 크기가 충분하지 않으면, 새로운 크기의 ArrayList를 생성하고 데이터를 복사하는 일이 발생하게 되므로 순차적으로 데이
+     * 터를 추가해도 ArrayList보다 LinkedList가 더 빠를 수 있다.
+     * 순차적으로 삭제한다는 것은 마지막 데이터로부터 역순으로 삭제해나간다는 것을 의미하며, ArrayList는 마지막 데이터부터 삭제할 경우 각
+     * 요소들의 재배치가 필요허지 않기 때문에 상당히 빠르다. 단지 마지막 요소의 값을 null로만 바꾸면 되니까.
+     */
+
+    /**
+     * 결론2. 중간 데이터를 추가/삭제하는 경우에는 LinkedList가 ArrayList보다 빠르다.
+     * 중간 요소를 추가 또는 삭제하는 경우, LinkedList는 각 요소간의 연결만 변경해주면 되기 때문에 처리속도가 상당히 빠르다. 반면에 Array
+     * List는 각 요소들을 재배치하여 추가할 공간을 확보하거나 빈 공간을 채워야하기 때문에 처리속도가 늦다.
      */
 
 }
--- src/chapter12/Chapter12_2.java
+++ src/chapter12/Chapter12_2.java
@@ -23,7 +23,34 @@ public class Chapter12_2 {
      * 된다. ArrayList<E>의 경우, 타입 변수 E는 'Element(요소)'의 첫글자를 따서 사용했다. 타입 변수가 여러 개인 경우에는 Map<K,V>와
      * 같이 콤마','를 구분자로 나열하면 된다. K는 Key(키)를 의미하고, V는 Value(값)을 의미한다. 무조건 'T'를 사용하기보다 가능하면, 이처럼
      * 상황에 맞게 의미있는 문자를 선택해서 사용하는 것이 좋다. 이들은 기호만 다를 뿐 '임이의 참조형 타입'을 의미한다는 것은 모두 같다.
-     *
+     * 기존에는 다양한 종류의 타입을 다루는 메서드의 매개변수나 리턴타입으로 Object타입의 참조변수를 많이 사용했고, 그로 인해 형변환이 불가피
+     * 했지만, 이젠 Object타입 대신 원하는 타입을 지정하기만 하면 되는 것이다.
+     * 이제 지네릭 클래스가 된 Box클래스의 객체를 생성할 때는 다음과 같이 참조변수와 생성자에 타입 T대신에 사용될 실제 타입을 지정해주어야
+     * 한다.
+     * Box<String> b = new Box<String>(); // 타입 T 대신, 실제 타입을 지정
+     * b.setItem(new Object()); // 에러, String 이외의 타입을 지정불가
+     * b.setItem("ABC"); // OK, String타입이므로 가능
+     * String item = b.getItem(); // 형변환이 필요없음
+     * 위의 코드에서 타입 T대신에 String타입을 지정해줬으므로, 지네릭 클래스 Box<T>는 다음과 같이 정의된 것과 같다.
+     * class Box { // 지네릭 타입을 String으로 지정
+     *  String item;
+     *  void setItem(String item) { this.item = item; }
+     *  String getItem() { return item; }
+     * }
      */
+
+    /**
+     * 지네릭스의 용어
+     * class Box<T> {}, Box: 원시타입, Box<T>: 지네릭 클래스
+     * Box<T>, 지네릭 클래스, 'T의 Box' 또는 'T Box'라고 읽는다.
+     * T, 타입 변수 또는 타입 매개변수.(T는 타입문자)
+     * Box, 원시 타입(raw type)
+     * 타입 문자 T는 지네릭 클래스 Box<T>의 타입 변수 또는 타입 매개변수라고 하는데, 메서드의 매개변수와 유사한 면이 있기 때문이다.
+     * 그래서 아래와 같이 타입 매개변수에 타입을 지정하는 것을 '지네릭 타입 호출'이라고 하고, 지정된 타입'String'을 '매개변수화된 타입(para
+     * meterized type)'이라고 한다.
+     * Box<String> b = new Box<String>();, String: 대입된 타입(매개변수화된 타입, parameterized type), Box<String>: 지네릭 타입
+     * 호출
+     */
+
 
 }
