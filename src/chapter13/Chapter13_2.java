be9af2e901a3b62c9e9e6e9466ddc666a0dc0187
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2024-12-22 23:02:17 +0900

    Study Day 11-2

--- src/chapter13/Chapter13_2.java
+++ src/chapter13/Chapter13_2.java
@@ -6,81 +6,72 @@ package chapter13;
 public class Chapter13_2 {
 
     /**
-     * 쓰레드를 구현하는 방법은 Thread클래스를 상속받는 방법과 Runnable인터페이스를 구
-     * 현하는 방법, 모두 두 가지가 있다. Thread클래스를 상속받으면 다른 클래스를 상속받을
-     * 수 없기 때문에, Runnable인터페이스를 구현하는
-     * 방법이 일반적이다.
-     * 1. Thread클래스를 상속
-     * class Mythread extends Thread {
-     *     public void run() { 작업내용 } Thread클래스의 run()을 오버라이딩
-     * }
-     * 2. Runnable인터페이스를 구현
-     * class MyThread implements Runnable {
-     *     public void run() { 작업내용 } Runnable인터페이스의 run()을 구현
-     * }
+     * <p>
+     *     쓰레드를 구현하는 방법은 {@code Thread}클래스를 상속받는 방법과 {@code Runnable}인터페이스를 구현하는 방법, 모두 두 가지가 있다.<br>
+     *     Thread클래스를 상속받으면 다른 클래스를 상속받을 수 없기 떄문에 , {@code Runnable}인터페이스를 구현하는 방법이 일반적이다.<br>
+     *     <b>1. Thread</b>클래스를 상속
+     *     <blockquote>
+     *         class MyThread extends Thread {
+     *             public void run() { 작업내용 } // {@code Thread}클래스의 run()을 오버라이딩
+     *         }
+     *     </blockquote><br>
+     *     <b>2. Runnable</b>인터페이스를 구현
+     *     <blockquote>
+     *         class MyThread implements Runnable {
+     *             public void run() { 작업내용 } // {@code Runnable}인터페이스의 run()을 구현
+     *         }
+     *     </blockquote><br>
+     *     {@code Runnable}인터페이스는 오로지 run()만 정의되어 있는 간단한 인터페이스이다.<br>
+     *     {@code Runnable}인터페이스를 구현하기 위해서 해야 할 일은 추상메서드인 run()의 몸통{}을 만들어 주는 것뿐이다.<br>
+     *     쓰레드를 구현한다는 것은, 위의 두 방법 중 어떤 것을 선택하든지, 그저 쓰레드를 통해 작업하고자 하는 내용으로 run()의 몸통{}을 채
+     *     우는 것일 뿐이다.
+     * </p>
      */
+    static class Memo01 {}
 
-    static class Ex1 {
+    /**
+     * <p>
+     *     {@code Thread}클래스를 상속받은 경우와 {@code Runnable}인터페이스를 구현한 경우의 인스턴스 생성방법이 다르다.<br>
+     *     <blockquote>
+     *         ThreadEx01_1 = t1 = new ThreadEx01_1(); // {@code Thread}의 자손클래스의 인스턴스를 생성<br>
+     *         Runnable r = new ThreadEx01_2(); // {@code Runnable}을 구현한 클래스의 인스턴스를 생성<br>
+     *         Thread t2 = new Thread(r); // 생성자 Thread(Runnable target)<br>
+     *         Thread t2 = new Thread(new ThreadEx01_2()); // 위의 두 줄을 한 줄로 간단히
+     *     </blockquote><br>
+     *      {@code Runnable}인터페이스를 구현한 경우, {@code Runnable}인터페이스를 구현한 클래스의 인스턴스를 생성한 다음, 이 인스턴스를
+     *      Thread클래스의 생성자의 매개변수로 제공해야 한다.
+     * </p>
+     */
+    static class TheadEx01 {
         public static void main(String[] args) {
-
-            Ex1_1 t1 = new Ex1_1();
-
-            Runnable r = new Ex1_2();
+            ThreadEx01_1 t1 = new ThreadEx01_1();
+            Runnable r = new ThreadEx01_2();
             Thread t2 = new Thread(r); // 생성자 Thread(Runnable target)
 
             t1.start();
             t2.start();
-
         }
     }
-
-    static class Ex1_1 extends Thread{
-       public void run() {
-           for(int i = 0; i < 5; i++) {
-               System.out.println("Thread Ex1_1 = " + getName()); // 조상인 Thread의 getName()을 호출
-           }
-       }
-    }
-
-    static class Ex1_2 implements Runnable {
+    
+    static class ThreadEx01_1 extends Thread {
         public void run() {
             for(int i = 0; i < 5; i++) {
-                // Thread.currentThread() - 현재 실행중인 Thread를 반환한다.
-                System.out.println("Thread Ex1_2 = " + Thread.currentThread().getName());
+                System.out.println(getName()); // 조상인 Thread의 getName()을 호출
             }
+        
         }
     }
 
-    /**
-     * Thread클래스를 상속받은 경우와 Runnable인터페이스를 구현한 경우의 인스턴스 생성
-     * 방법이 다르다.
-     * Ex1_1 t1 = new Ex1_1(); Thread의 자손 클래스의 인스턴스를 생성
-     *
-     * Runnable r = new Ex1_2(); Runnable을 구현한 클래스의 인스턴스를 생성
-     * Thread t2 = new Thread(r); 생성자 Thread(Runnable target)
-     *
-     * Thread t2 = new Thread(new Ex1_2());
-     * Runnable인터페이스를 구현한 경우, Runnable인터페이스를 구현한 클래스의 인스턴스
-     * 를 생성한 다음, 이 인스턴스를 Thread클래스의 생성자의 매개변수로 제공해야 한다.
-     * Thread클래스를 상속받으면, 자손 클래스에서 조상인 Thread클래스의 메서드를 직접 호출할 수 있지만, Runnable을 구현하면 Thread클래
-     * 스의 static메서드인 currentThread()를 호출하여 쓰레드에 대한 참조를 얻어 와야만 호출이 가능하다.
-     * static Thread currentThread() - 현재 실행중인 쓰레드의 참조를 반환한다.
-     * String getName() - 쓰레드의 이름을 반환한다.
-     * 쓰레드의 실행 - start()
-     * 쓰레드를 생성했다고 해서 자동으로 실행되는 것은 아니다. start()를 호출해야만 쓰레드가 실행된다.
-     * t1.start(); // 쓰레드 t1을 실행시킨다.
-     * t2.start(); // 쓰레드 t2를 실행시킨다.
-     * 사실은 start()가 호출되었다고 해서 바로 실행되는 것이 아니라. 일단 실행대기 상태에 있다가 자신의 차례가 되어야 실행된다. 물론 실행
-     * 대기중인 쓰레드가 하나도 없으면 곧바로 실행상태가 된다.
-     * ※ 쓰레드의 실행순서는 OS의 스케쥴러가 작성한 스케쥴에 의해 결정된다.
-     * 한 가지 더 알아 두어야 하는 것은 한 번 실행이 종료된 쓰레드는 다시 실행할 수 없다는 것이다. 즉, 하나의 쓰레드에 대해 start()가
-     * 한 번만 호출될 수 있다는 뜻이다. 만일 쓰레드의 작업을 한 번 더 수행해야 한다면 아래의 오른쪽 코드와 같이 새로운 쓰레드를 생성한
-     * 다음에 start()를 호출해야 한다. 하나의 쓰레드에 대해 start()를 두 번 이상 호출하면 실행시에 IllegalThreadStateException이 발
-     * 생한다.
-     * ThreadEx1_1 = new ThreadEx1_1();    ThreadEx1_1 t1 = new ThreadEx1_1();
-     * t1.start();                      →  t1.start();
-     * t1.start(); // 예외 발생             t1 = new ThreadEx1_1(); // 다시 생성
-     *                                     t1.start(); // OK
-     */
+    static class ThreadEx01_2 implements Runnable {
+
+        @Override
+        public void run() {
+            for(int i = 0; i < 5; i++) {
+                // Thead.currentThread() - 현재 실행중인 Thread를 반환한다.
+                System.out.println(Thread.currentThread().getName());
+            }
+        }
+
+    }
 
 }
