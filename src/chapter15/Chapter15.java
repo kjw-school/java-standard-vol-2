d825263b3acab0f63704b493ae579595e1a43dc1
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2025-07-09 02:25:24 +0900

    Day 42 - Chapter10, Chapter10_2,Chapter15, Chapter15_2

--- src/chapter15/Chapter15.java
+++ src/chapter15/Chapter15.java
@@ -112,9 +112,127 @@ public class Chapter15 {
 	 * <h5>1.4 보조 스트림</h5><br>
 	 * 스트림의 기능을 보완하기 위한 보조스트림이 제공된다. 보조스트림은 실제 데이터를 주고받는 스트림이 아니기 때문에 데이터를 입출력할 수 있는 기능은 없지만,<br>
 	 * 스트림의 기능을 향상시키거나 새로운 기능을 추가할 수 있다. <br>
-	 * 그래서 보조스트림만으로는 입출력을 처리할 수 없고, 스트림을 먼저 생성한 다음에 이를 이용해서 보조스트림을 생성해야 한다.
+	 * 그래서 보조스트림만으로는 입출력을 처리할 수 없고, 스트림을 먼저 생성한 다음에 이를 이용해서 보조스트림을 생성해야 한다.<br>
+	 * <table>
+	 *     <thead>
+	 *         <th>입력</th>
+	 *         <th>출력</th>
+	 *         <th>설명</th>
+	 *     </thead>
+	 *     <tbody>
+	 *         <tr>
+	 *             <td>FilterInputStream</td>
+	 *             <td>FilterOutputStream</td>
+	 *             <td>필터를 이용한 입출력 처리</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>BufferedInputStream</td>
+	 *             <td>BufferedOutputStream</td>
+	 *             <td>버퍼를 이용한 입출력 성능향상</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>DataInputStream</td>
+	 *             <td>DataOutputStream</td>
+	 *             <td>int, float와 같은 기본형 단위(primitive type)로 데이터를 처리하는 기능</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>SequenceInputStream</td>
+	 *             <td>없음</td>
+	 *             <td>두 개의 스트림을 하나로 연결</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>LineNumberInputStream</td>
+	 *             <td>없음</td>
+	 *             <td>읽어 온 데이터의 라인 번호를 카운트(JDK1.1 부터 LineNumberReader로 대체)</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>ObjectInputStream</td>
+	 *             <td>ObjectOutputStream</td>
+	 *             <td>데이터를 객체단위로 읽고 쓰는데 사용, 주로 파일을 이용하며 객체 직렬화와 관련있음</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>없음</td>
+	 *             <td>PrintStream</td>
+	 *             <td>버퍼를 이용하며, 추가적인 print관련 기능(print, printf, println메서드)</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>PushbackInputStream</td>
+	 *             <td>없음</td>
+	 *             <td>버퍼를 이용해서 읽어 온 데이터를 다시 되돌리는 기능(unread, push back to buffer)</td>
+	 *         </tr>
+	 *     </tbody>
+	 * </table>
 	 */
 	class Memo4 {
+
+	}
+
+	/**
+	 * <h5>1.5 문자기반 스트림 - Reader, Writer</h5><br>
+	 * 바이트기반이라 함은 입출력 단위가 1 byte라는 뜻이다. 이미 알고 있는 것과 같이 C언어와 달리 Java에서는 한문자를 의미하는 char형이 1 byte가 아니라 2 byte이기 때문에 바이트기반의 스트림으로 2 byte인 문자를 처리하는 데는 어려움이 있다.<br>
+	 * <pre><code>
+	 *     InputStream -> Reader
+	 *     OutputStream -> Writer
+	 * </code></pre>
+	 * <br>
+	 * <table>
+	 *     <thead>
+	 *         <th>바이트기반 스트림</th>
+	 *         <th>문자기반 스트림</th>
+	 *     </thead>
+	 *     <tbody>
+	 *		   <tr>
+	 *		       <td>FileInputStream, FileOutputStream</td>
+	 *		       <td>FileReader, FileWriter</td>
+	 *		   </tr>
+	 *		   <tr>
+	 *		       <td>ByteArrayInputStream, ByteArrayOutputStream</td>
+	 *		       <td>CharArrayReader, CharArrayWriter</td>
+	 *		   </tr>
+	 *		   <tr>
+	 *		       <td>PipedInputStream, PipedOutputStream</td>
+	 *		       <td>PipedReader, PipedWriter</td>
+	 *		   </tr>
+	 *		   <tr>
+	 *		       <td>StringBufferInputStream(deprecated), StringBufferOutputStream(deprecated)</td>
+	 *		       <td>StringReader, StringWriter</td>
+	 *		   </tr>
+	 *     </tbody>
+	 * </table>
+	 * <br>
+	 * 문자기반 스트림의 이름은 바이트기반 스트림의 이름에서 InputStream은 Reader로 OutputStream은 Writer로만 바꾸면 된다.<br>
+	 * 단, ByteArrayInputStream에 대응하는 문자기반 스트림은 char배열을 사용하는 CharArrayReader이다.<br>
+	 * 보조스트림 역시 다음과 같은 문자 기반 보조스트림이 존재하며 사용목적과 방식은 바이트기반 보조스트림과 다르지 않다.<br>
+	 * <table>
+	 *     <thead>
+	 *         <th>바이트기반 보조스트림</th>
+	 *         <th>문자기반 보조스트림</th>
+	 *     </thead>
+	 *     <tbody>
+	 *         <tr>
+	 *             <td>BufferedInputStream, BufferedOutputStream</td>
+	 *             <td>BufferedReader, BufferedWriter</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>FilterInputStream, FilerOutputStream</td>
+	 *             <td>FilterReader, FilterWriter</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>LineNumberInputStream(deprecated)</td>
+	 *             <td>LineNumberReader</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>PrintStream</td>
+	 *             <td>PrintWriter</td>
+	 *         </tr>
+	 *         <tr>
+	 *             <td>PushbackInputStream</td>
+	 *             <td>PushbackReader</td>
+	 *         </tr>
+	 *     </tbody>
+	 * </table>
+	 */
+	class Memo5 {
 
 	}
 
---
+++ src/chapter15/Chapter15_2.java
@@ -0,0 +1,16 @@
+package chapter15;
+
+/**
+ * <h1>2. 바이트기반 스트림</h1>
+ */
+public class Chapter15_2 {
+
+	/**
+	 * <h5>2.1 InputStream과 OutputStream</h5><br>
+	 * InputStream과 OutputStream은 모든 바이트기반의 스트림의 조상이다.<br>
+	 */
+	class Memo1 {
+
+	}
+
+}
