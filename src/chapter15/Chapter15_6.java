e99fb1134a86b842473d48affac488e87a37d1b9
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2025-08-28 23:38:42 +0900

    Day49 - Chapter15_6

--- src/chapter15/Chapter15_6.java
+++ src/chapter15/Chapter15_6.java
@@ -1,5 +1,9 @@
 package chapter15;
 
+import static java.lang.System.*;
+
+import java.io.IOException;
+
 /**
  * <h1>6. 표준입출력과 File</h1>
  */
@@ -17,10 +21,39 @@ public class Chapter15_6 {
 	 * </code></pre>
 	 * <br>
 	 * System클래스이 소스에서 알 수 있듯이 in, out, err은 System클래스에 선언된 클래스변수(static변수)이다.<br>
-	 * 선언부분만을 봐서는 out, err, in의 탕비은 InputStream과 PrintStream이지만 실제로는 버퍼를 이용하는 BufferedInputStream과 BufferedOutputStream의 인스턴스를 사용한다.
+	 * 선언부분만을 봐서는 out, err, in의 탕비은 InputStream과 PrintStream이지만 실제로는 버퍼를 이용하는 BufferedInputStream과 BufferedOutputStream의 인스턴스를 사용한다.<br>
+	 * 콘솔입력은 버퍼를 가지고 있기 때문에 Backspace키를 이용해서 편집이 가능하며 한 번에 버퍼의 크기만큼 입력이 가능하다.<br>
+	 * 그래서 Enter키나 입력의 끝을 알리는 '^z'를 누르기 전까지는 아직 데이터가 입력 중인 것으로 간주되어 커서가 입력을 계속 기다리는 상태(블락킹 상태)에 머무르게 된다.<br>
+	 * 콘솔에 데이터를 입력하고 Enter키를 누르면 입력대기상태에서 벗어나 입력된 데이터를 읽기 시작하고 입력된 데이터를 모두 읽으면 다시 입력대기 상태가 된다.<br>
+	 * 이러한 과정이 반복되다가 사용자가 '^z'를 입력하면, read()는 입력이 종료되었음을 인식하고 -1을 반환하여 while문을 벗어나 프로그램이 종료된다.<br>
+	 * Enter키를 누르는 것은 두 개의 특수문자 '\r'과 '\n'이 입력된 것으로 간주된다. '\r'은 캐리지리턴(carriage return),즉 커서를 현재 라인의 첫 번째 컬럼으로 이동시키고<br>
+	 * '\n'은 커서를 다음 줄로 이동시키는 줄바꿈(new line)을 한다.<br>
+	 * 그래서 Enter키를 누르면, 캐리지리턴과 줄바꿈이 수행되어 다음 줄의 첫 번째 칼럼으로 커서가 이동하는 것이다.<br>
+	 * 여기서 한 가지 문제는 Enter키도 사용자입력으로 간주되어 매 입력마다 '\r'과 '\n'이 붙기 때문에 이 들을 제거해주어야 하는 불편함이 있다는 것이다.<br>
+	 * 이러한 불편함을 제거하려면 전에 살펴본 것과 같이 System.in에 BufferedReader를 이용해서 readLine()을 통해 라인단위로 데이터를 입력받으면 된다.<br>
+	 * 텍스트기반의 사용자인터페이스 시대에 탄생한 C언어는 콘솔이 데이터를 입력받는 주요 수단이었지만, 자바가 탄생한 그래픽기반의 사용자인터페이스 시대는 콘솔을 통해서 데이터를 입력받는 경우는 드물기 때문에 Java에서 콘솔을 통한 입력에 대한 지원이 미약했다.<br>
+	 * 나중에 Scanner와 Consoel같은 클래스가 추가되면서 많이 보완되었다.
+	 *
 	 */
 	class Memo1 {
 
+	}
+
+	public static class StandardIOEx1 {
+		public static void main(String[] args) {
+
+			try {
+				int input = 0;
+
+				while ((input = System.in.read()) != -1) {
+					out.println("input :" + input + ", (char)input:" + (char)input);
+				}
+
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
 	}
 
 }
